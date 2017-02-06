9864830c0ec0ce3d7ef89a0b9ecf7d606e7de7fc
ann2bad <ann2bad@me.com>
2017-02-07 02:03:49 +0800

    for cl4

--- Learn to code 1 - Fundamentals of Swift/Learn to Code 1.playgroundbook/Contents/Chapters/Document3.playgroundchapter/Pages/Challenge4.playgroundpage/Contents.swift
+++ Learn to code 1 - Fundamentals of Swift/Learn to Code 1.playgroundbook/Contents/Chapters/Document3.playgroundchapter/Pages/Challenge4.playgroundpage/Contents.swift
@@ -6,9 +6,9 @@
 //
 //#-end-hidden-code
 /*:
- **Challenge:** Decompose multiple patterns into functions and loops.
+ **挑战：** 将重复的模式分解为函数和 for 循环。
  
-In this challenge, there are gems to collect and switches to toggle open. You'll need to identify the patterns for gem collection and switch activation. Then you’ll write a [function](glossary://function) for each [pattern](glossary://pattern) and figure out how many times you’ll need to [call](glossary://call) those functions using loops.
+在这个挑战中，你要收集到所有的宝石并激活所有的开关。先想好收集宝石和激活开关的模式，然后为每个 [模式](glossary://pattern) 分别写出 [函数](glossary://function)，最后想想使用循环 [调用](glossary://call) 这些函数的次数。
 
 */
 //#-hidden-code
@@ -17,7 +17,7 @@ playgroundPrologue()
 //#-code-completion(everything, hide)
 //#-code-completion(currentmodule, show)
 //#-code-completion(identifier, show, moveForward(), turnLeft(), collectGem(), toggleSwitch(), turnRight(), func, for)
-//#-editable-code Tap to enter code
+//#-editable-code 点这里输入代码
 
 //#-end-editable-code
 //#-hidden-code
--- Learn to code 1 - Fundamentals of Swift/Learn to Code 1.playgroundbook/Contents/Chapters/Document3.playgroundchapter/Pages/Challenge4.playgroundpage/Sources/Assessments.swift
+++ Learn to code 1 - Fundamentals of Swift/Learn to Code 1.playgroundbook/Contents/Chapters/Document3.playgroundchapter/Pages/Challenge4.playgroundpage/Sources/Assessments.swift
@@ -3,7 +3,7 @@
 //
 //  Copyright (c) 2016 Apple Inc. All Rights Reserved.
 //
-let success = "### Unbelievable! \nLook how fast you're becoming a coder! You've learned the secret: break these tough-looking problems into little pieces, then group your simple solutions into a [function](glossary://function) that you can [call](glossary://call) over and over again. \n\n[**Next Page**](@next)"
+let success = "### 真是不可思议！ \n你已经学会如何将复杂问题分解，然写出 [函数](glossary://function) ，通过反复 [调用](glossary://call) 来解决问题。 \n\n[**下一关**](@next)"
 
 let solution: String? = nil
 
@@ -13,9 +13,9 @@ public func assessmentPoint() -> AssessmentResults {
     pageIdentifier = "Gem_Farm"
 
     var hints = [
-        "First, think of all of the tasks you can include in a function.",
-        "How can you use `for` loops to repeat a set of functions that you've written?",
-        "This puzzle is a **challenge** and has no provided solution. Strengthen your coding skills by creating your own approach to solving it."
+        "首先思考你要将这个挑战分解成哪些小任务？",
+        "你要如何使用 `for` 循环来重复执行你写出的函数呢？",
+        "这是一个 **挑战关** ，并不会提供答案。尝试着独立找出解决问题的方案，会对你编程技能有很大的提升。"
         ]
     
     switch currentPageRunCount {
@@ -23,9 +23,9 @@ public func assessmentPoint() -> AssessmentResults {
     case 0..<4:
         break
     case 4:
-        hints[0] = "Remember, this puzzle is a **Challenge**. It might take you a few more tries to get to the solution."
+        hints[0] = "这是一个 **挑战关** ，你可能要多试几次才能通过。"
     case 5...7:
-        hints[0] = "**Tip**: If you’re struggling with a challenge, it may help to take your mind off of it and come back to it later. In the meantime, feel free to move on."
+        hints[0] = "**提示**：当你面临挑战的时候，你可能需要放松一会再回来继续。"
     default:
        break
 
